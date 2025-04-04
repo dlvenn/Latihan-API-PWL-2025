60ec1bb16800fda5e14d68baebe18fe9db7ba860
dlvenn <desyanalavenia6@gmail.com>
2025-04-04 22:37:48 +0700

    post & put

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let products = [
@@ -17,6 +18,60 @@ app.get("/products", (req, res) => {
         data: products
     });
 });
+
+app.post("/products", (req, res) => {
+    const { name, price } = req.body;
+
+    if (!name || price === undefined) {
+        return res.status(400).json ({
+            status: "error",
+            message: "Field 'name' dan 'price' wajib diisi"
+        });
+    }
+
+    const newProduct = {
+        id: products.length + 1,
+        name, 
+        price
+    };
+
+    products.push(newProduct);
+
+    res.status(201).json ({
+        status: "success",
+        data: newProduct
+    });
+});
+
+app.put("/products/:id", (req, res) => {
+    const id = parseInt (req.params.id);
+    const { name, price } = req.body;
+
+    const productIndex = products.findIndex (p => p.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({ status: "error", message: "Produk tidak ditemukan" });
+    }
+    if (!name || price === undefined || price === "") {
+        return res.status(400).json({
+            status: "error",
+            message: "Field 'name' dan 'price' wajib diisi"
+        });
+    }
+
+    products [productIndex] = {
+        id,
+        name,
+        price: parseFloat(price)
+    };
+
+    res.json ({
+        status: "success",
+        message: "Produk berhasil diperbarui",
+        data: products[productIndex]
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
